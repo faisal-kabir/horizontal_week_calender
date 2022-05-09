26f645d5ee3749dd17ab2ad6dd5a5e6252328ad5
Faisal <faisalkabir95@gmail.com>
2022-05-09 11:33:28 +0600

    Fixed horizontal_week_calender_web import issue

---
+++ lib/horizontal_week_calender_web.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class HorizontalWeekCalenderWeb extends StatefulWidget {
+  const HorizontalWeekCalenderWeb({Key? key}) : super(key: key);
+
+  @override
+  State<HorizontalWeekCalenderWeb> createState() => _HorizontalWeekCalenderWebState();
+
+  static void registerWith(Registrar registrar) {
+
+  }
+}
+
+class _HorizontalWeekCalenderWebState extends State<HorizontalWeekCalenderWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
